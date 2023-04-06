9640f1a4e0fad15e62bbb13a310e6071f0c184e5
KmK0708 <kample2k@gmail.com>
2023-04-06 18:54:34 +0900

    4월6일 블루프린트 수정
    위치나 세부사항들...

--- WhatTheBoxProject.bat
+++ WhatTheBoxProject.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\SBAuser\Documents\Unreal Projects\WhatTheBoxProject\WhatTheBoxProject.uproject" -game -log -ResX=1280 -ResY=720
+"C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\SBAuser\Desktop\UEProject\WhatTheBoxProject\WhatTheBoxProject.uproject" -game -log -ResX=1280 -ResY=720
